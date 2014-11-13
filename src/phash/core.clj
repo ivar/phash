33bd8d49fefdda0bafa7ef55762da151f4c4c6b3
Ladislav Gubik <lacogubik@gmail.com>
2014-11-13 08:26:31 +0000

    Innit

---
+++ src/phash/core.clj
@@ -0,0 +1,6 @@
+(ns phash.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/phash/core_test.clj
@@ -0,0 +1,7 @@
+(ns phash.core-test
+  (:require [clojure.test :refer :all]
+            [phash.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
